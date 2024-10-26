497cf58ac4874021034e4a77f79233a63432a966
Sonyex24 <imamweah09@gmail.com>
2024-10-26 14:19:42 +0700

    Pesan commit yang menjelaskan perubahan

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dashboard.dart'; // Impor file dashboard.dart
 
+//tes commit
+
 void main() {
   runApp(const MainApp());
 }
